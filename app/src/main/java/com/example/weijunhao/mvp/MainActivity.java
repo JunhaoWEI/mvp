3c1c56ca998f88b6c66a28ec8a4d01de0a1f57c0
WEI JUNHAO <junhao9408@gmail.com>
2017-05-11 18:05:04 +0800

    mvvm

--- app/src/main/java/com/example/weijunhao/mvp/MainActivity.java
+++ app/src/main/java/com/example/weijunhao/mvp/MainActivity.java
@@ -2,15 +2,70 @@ package com.example.weijunhao.mvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButton = (Button) findViewById(R.id.button);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeView();
+            }
+        });
+
+    }
+
+    private void changeView() {
+        //被观察者
+        Observable<String> myObservavble = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        subscriber.onNext("Hello RxAndroid");
+                        subscriber.onCompleted();
+                    }
+                }
+        );
+
+        //
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "更换内容成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "更换内容失败", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(String s) {
+                mTextView.setText(s);
+            }
+        };
+
+        //订阅事件
+        myObservavble.observeOn(AndroidSchedulers.mainThread()).subscribe(mySubscriber);
     }
 }
